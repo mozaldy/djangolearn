5e85fe96c570a3e316da16ec1a60f31d44ef7e28
aws <moxaldy@gmail.com>
2023-02-22 16:18:51 +0700

    boilerplate initialization

---
+++ firstapp/hello/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'hello/index.html', {
+        'name': 'world!'
+    })
+
+def greet(request, name):
+    return render(request, 'hello/index.html', {
+        'name': name.capitalize()
+    })
